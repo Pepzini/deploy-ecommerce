d3c2a5487e23657983cb842f0fb8b32dcdcc1d3e
Pepzini <jideniniola@gmail.com>
2022-01-31 09:16:48 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://github.com/Pepzini/deploy-ecommerce-api/blob/main/index.php'
+  apiURL: 'https://github.com/Pepzini/deploy-ecommerce-api/'
 };

c515a92bd514134ee577251e38ffd83e47efef87
Pepzini <jideniniola@gmail.com>
2022-04-04 12:05:31 +0100

    login changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +11,28 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private apiURL = environment.apiURL;
    loginForm = new FormGroup({
     username: new FormControl(),
     password: new FormControl()
   });
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private http: HttpClient, private router: Router) { }
+  // login() {
+  //   this.authService.login(this.loginForm.value);
+  // }
   login() {
-    this.authService.login(this.loginForm.value);
+    this.http.post<any>(this.apiURL + 'auth/user/login', this.loginForm.value).subscribe(
+      (response) => {
+        console.log('login:', response);
+        if (response.status == 'success') {
+          this.authService.login(response.user);
+          this.router.navigate(['/']);
+        }
+      },
+      (error) => {
+        console.log('Server Error:', error);
+      }
+    );
   }
   ngOnInit(): void {
   }

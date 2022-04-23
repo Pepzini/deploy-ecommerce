87cb35a8483c63c361f43af2fff2121ee2255d1d
Pepzini <jideniniola@gmail.com>
2022-04-23 11:37:25 +0100

    adjust login page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'manage-ecommerce'`, () => {
+  it(`should have as title 'deploy-ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('manage-ecommerce');
+    expect(app.title).toEqual('deploy-ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('manage-ecommerce app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('deploy-ecommerce app is running!');
   });
 });
